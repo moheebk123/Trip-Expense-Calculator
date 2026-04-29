5cdb1ada0f645a18a445616d1d220f0bee409b90
Moheeb <moheebk522@gmail.com>
2026-04-29 16:30:16 +0530

    add wrappers, header, and context component in app

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/common/Header";
+import PersonsBox from "@/components/person/PersonsBox";
+import ExpensesBox from "@/components/expenses/ExpensesBox";
+import StatsBox from "./components/stats/StatsBox";
+import ExpenseContextProvider from "@/context/ExpenseContextProvider";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-slate-950 via-slate-900 to-slate-800 text-white flex flex-col items-center p-10">
+      <Header />
+      <ExpenseContextProvider>
+        <PersonsBox />
+        <ExpensesBox />
+        <StatsBox />
+      </ExpenseContextProvider>
+    </div>
+  );
+}
+
+export default App;
