a1efd48103ea441b73b195218a91e34dc68e065a
Moheeb <moheebk522@gmail.com>
2026-05-15 01:28:30 +0530

    add packages and define global interfaces

---
+++ src/types/index.d.ts
@@ -0,0 +1,57 @@
+import type { LucideIcon } from "lucide-react";
+
+interface AlertInterface {
+  show: boolean;
+  variant: "error" | "success" | "warning";
+  message: string;
+}
+
+interface LoadInterface {
+  isLoading: boolean;
+}
+
+interface PersonInterface {
+  id: number;
+  name: string;
+}
+
+interface TripInterface {
+  id: number;
+  title: string;
+  members: PersonInterface[];
+  perPersonBudget: number;
+}
+
+interface ExpenseInterface {
+  id: number;
+  title: string;
+  amount: number;
+  participants: PersonInterface[];
+  trip: number;
+  date: string;
+  paidBy?: PersonInterface;
+  paidAmount?: number;
+}
+
+interface ExpenseDataInterface {
+  expenses: ExpenseInterface[];
+  trips: TripInterface[];
+}
+
+interface DrawerMenuInterface {
+  id: number;
+  title: string;
+  url: string;
+  icon: LucideIcon;
+}
+
+interface DrawerItemInterface {
+  id: number;
+  menuHeading: string;
+  menuItems: DrawerMenuInterface[];
+}
+
+interface LegalInterface {
+  heading: string;
+  paragraph: string;
+}
