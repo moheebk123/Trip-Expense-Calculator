06d126dd8f46d7908d3a3921417b423623354bfa
Moheeb <moheebk522@gmail.com>
2026-05-15 01:34:53 +0530

    add components in index

---
+++ src/components/index.ts
@@ -0,0 +1,34 @@
+// Common
+import Loader from "./common/Loader";
+import AlertBox from "./common/AlertBox";
+import Links from "./common/Links";
+import AppDrawer from "./common/AppDrawer";
+
+// Trips
+import Header from "./trips/Header";
+import Remove from "./trips/Remove";
+import AddTrip from "./trips/AddTrip";
+import TripsBox from "./trips/TripsBox";
+
+// Trip
+import TripNotFound from "./trip/TripNotFound";
+import TripStats from "./trip/TripStats";
+import TripMembers from "./trip/TripMembers";
+import AddExpense from "./trip/AddExpense";
+import TripExpenses from "./trip/TripExpenses"
+
+export {
+  Loader,
+  AlertBox,
+  Links,
+  AppDrawer,
+  Header,
+  Remove,
+  AddTrip,
+  TripsBox,
+  TripNotFound,
+  TripStats,
+  TripMembers,
+  AddExpense,
+  TripExpenses
+};
